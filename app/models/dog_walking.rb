4ee0671237c516506f52b7d6f7c0999caefe001d
almirpask <almir.santos@umanni.com.br>
2018-11-19 04:41:50 -0200

    Add model validations to DogWalking

--- app/models/dog_walking.rb
+++ app/models/dog_walking.rb
@@ -1,5 +1,12 @@
 class DogWalking < ApplicationRecord
+  validates :duration,
+            inclusion: { in: [30, 60], allow_nil: false, only_integer: true },
+            presence: true
+  validates :pets, numericality: { only_integer: true }, presence: true
+
   enum status: { pending: 0, in_progress: 1, finished: 2 }
 
-  scope :next_walks, -> { where('status != ? AND scheduled_day >= ?', 2, Date.today.to_datetime) }
+  scope :next_walks, lambda {
+    where('status != ? AND scheduled_day >= ?', 2, Date.today)
+  }
 end
--- db/migrate/20181118221545_create_dog_walkings.rb
+++ db/migrate/20181118221545_create_dog_walkings.rb
@@ -4,7 +4,7 @@ class CreateDogWalkings < ActiveRecord::Migration[5.2]
       t.integer :status, :default => 0
       t.integer :pets
       t.float :price
-      t.float :duration
+      t.integer :duration
       t.float :latitude
       t.float :longitude
       t.time :start
